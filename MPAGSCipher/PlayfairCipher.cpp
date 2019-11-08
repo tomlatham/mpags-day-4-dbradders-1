3ea2a9c2702339031c3455a2351955b104d22be6
David Bradley <d.bradley.1@warwick.ac.uk>
2019-11-08 13:41:43 +0000

    Created PlayfairCipher.cpp skeleton

---
+++ MPAGSCipher/PlayfairCipher.cpp
@@ -0,0 +1,20 @@
+#inlcude <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+#include "PlayfairCipher.hpp"
+
+PlayfairCipher::PlayfairCipher (const std::string& key)
+{
+
+}
+
+void PlayfairCipher::setKey(const std::string& key)
+{
+
+}
+
+std::string PlayfairCipher::applyCipher(const std::string& inputText,
+					const CipherMode cipherMode) const
