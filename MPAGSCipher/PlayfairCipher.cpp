10efea1fe4ba610730b2a3423921facfbca9796d
David Bradley <d.bradley.1@warwick.ac.uk>
2019-11-08 13:51:22 +0000

    Fixed some errors

--- MPAGSCipher/PlayfairCipher.cpp
+++ MPAGSCipher/PlayfairCipher.cpp
@@ -1,10 +1,11 @@
-#inlcude <string>
+#include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include <iterator>
 
 #include "PlayfairCipher.hpp"
+#include "CipherMode.hpp"
 
 PlayfairCipher::PlayfairCipher (const std::string& key)
 {
@@ -17,4 +18,4 @@ void PlayfairCipher::setKey(const std::string& key)
 }
 
 std::string PlayfairCipher::applyCipher(const std::string& inputText,
-					const CipherMode cipherMode) const
+					const CipherMode cipherMode) const;
